605b0ee825fbf01ddeb1a2be1ba5379b1d0283b3
jhcho <jhcho@DEV161121-PC>
2016-11-28 16:49:43 +0900

    added CustomMapArgumentReolver

--- jhcho/src/main/java/com/example/jhcho/sample/controller/SampleController.java
+++ jhcho/src/main/java/com/example/jhcho/sample/controller/SampleController.java
@@ -1,7 +1,9 @@
 package com.example.jhcho.sample.controller;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.annotation.Resource;
 
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.jhcho.common.CommandMap;
 import com.example.jhcho.sample.service.SampleService;
 
 @Controller
@@ -30,11 +33,19 @@ public class SampleController {
     }
     
     
-    @RequestMapping("/sample/example")
-    public ModelAndView exampleList() throws Exception {
-    	ModelAndView mv = new ModelAndView();
-    	mv.setViewName("/sample/sampleList");
-    	
-    	return mv;
+    @RequestMapping(value="/sample/resolver")
+    public ModelAndView sampleResolver(CommandMap commandMap) throws Exception{
+        ModelAndView mv = new ModelAndView("");
+
+        System.out.println("----->");
+        if(commandMap.isEmpty() == false){
+            Iterator<Entry<String,Object>> iterator = commandMap.getMap().entrySet().iterator();
+            Entry<String,Object> entry = null;
+            while(iterator.hasNext()){
+                entry = iterator.next();
+                System.out.println("key : "+entry.getKey()+", value : "+entry.getValue());
+            }
+        }
+        return mv;
     }
 }
